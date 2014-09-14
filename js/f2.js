d813673e4ae34aeba449f48ce4bc1881cac33dda
readersJunction <readersjunction@outlook.com>
2014-09-14 21:09:20 +0530

    Upload

---
+++ js/f2.js
@@ -0,0 +1,13 @@
+$(function() {
+    $(".mostPopular").jCarouselLite({
+        btnNext: ".next",
+        btnPrev: ".prev"
+    });
+	
+	$(".topRated").jCarouselLite({
+        btnNext: ".next2",
+        btnPrev: ".prev2"
+    });
+
+	
+	});
