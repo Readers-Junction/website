4cc596549111c79d42f3eaeb7ecf7dbafea9d656
HarshSharma12 <mail.hs.harsh@gmail.com>
2014-09-19 07:02:27 +0530

    Removed some test files

--- getCat.js
+++
@@ -1,9 +0,0 @@
-function getimg(){
- var a=window.location.href.split('#')[1],
- id=a[0].split('=')[1],
- alert("asds");
- src=a[1].split('=')[1],
- img=document.images[0];
- img.id=id;
- img.src=src;	
-}
